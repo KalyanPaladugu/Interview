78944f432ed5013fb8ae601a4ddf1ccbc46bbd1c
KalyanPaladugu <kalyanchakravarthi.1994@gmail.com>
2021-10-21 11:42:45 +0530

    HOC

--- practice/src/App.js
+++ practice/src/App.js
@@ -3,11 +3,13 @@ import PersonsList  from './components/PersonsList';
 import PersonInput from './components/PersonInput';
 import API from './components/API';
 import Parent from './components/Parent';
+import ClickCounter from './components/ClickCounter';
+import HoverCounter from './components/HoverCounter';
 function App() {
   return (
     <div className="App">
-     <Parent />
-  
+     <ClickCounter />
+  <HoverCounter />
     </div>
   );
 }
