ad7e8d8d45830b07bc93bafccfad4833c6a95736
KalyanPaladugu <kalyanchakravarthi.1994@gmail.com>
2021-10-22 13:42:38 +0530

    Datafetch with id on button click

--- practice/src/components/DataFetching.js
+++ practice/src/components/DataFetching.js
@@ -4,15 +4,15 @@ export default function DataFetching() {
 
     const [post,setPost]=useState({})
     const [id,setId]=useState(1)
-
+    const [idfromButtonClick,setIdfromButtonClick]=useState(1)
     useEffect(()=>{
-        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`).then(
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${idfromButtonClick}`).then(
             res=>
              setPost(res.data)
         ).catch(
             error => console.log(error)
         )
-    })
+    },[idfromButtonClick])
     return (
         <div>
             <input type="text" value={id} onChange={(e)=> setId(e.target.value) } />
@@ -24,6 +24,7 @@ export default function DataFetching() {
                 }
             </ul> */}
             <div>{post.title}</div>
+            <button type="button" onClick={()=>{setIdfromButtonClick(id)}}>from button</button>
         </div>
     )
 }
