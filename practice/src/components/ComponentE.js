1d4b5f3a151b4d4b9187a7e8d163a589905d690f
KalyanPaladugu <kalyanchakravarthi.1994@gmail.com>
2021-10-22 15:33:57 +0530

    Context api using

--- practice/src/components/ComponentE.js
+++ practice/src/components/ComponentE.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import ComponentF from './ComponentF'
-
+import { useContext } from 'react'
+import { UserContext,ChanelContext } from '../App'
 export default function ComponentE() {
+    const user=useContext(UserContext)
+    const chanel=useContext(ChanelContext)
     return (
         <div>
-          <ComponentF />
+         {user} chanel is {chanel}
         </div>
     )
 }
