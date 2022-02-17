df66fafaae1280609c71e3f65fd3093421c2d74d
KalyanPaladugu <31949037+KalyanPaladugu@users.noreply.github.com>
2022-02-17 06:57:44 +0530

    Lifecycle methods & Forms

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import style from './style';
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{...style,...style.heading1}}> Welcome</h1>
+    </div>
+  );
+}
+
+export default App;
