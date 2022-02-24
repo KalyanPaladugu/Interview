38c0f0497371bddbcc5ff6017d43a8b8e8e994f0
KalyanPaladugu <31949037+KalyanPaladugu@users.noreply.github.com>
2022-02-24 15:55:13 +0530

    HOC & Pure component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import style from './style';
+import Pure from './Pure';
+import Home from './Home';
 function App() {
   return (
     <div className="App">
-      <h1 style={{...style,...style.heading1}}> Welcome</h1>
+      {/* <h1 style={{...style,...style.heading1}}> Welcome</h1> */}
+   
+    {/* <Pure /> */}
+    <Home />
     </div>
   );
 }
