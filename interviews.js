eec5a645333f505cd639076b28c69595e32791b4
KalyanPaladugu <31949037+KalyanPaladugu@users.noreply.github.com>
2022-03-23 15:30:43 +0530

    add

--- interviews.js
+++ interviews.js
@@ -19,3 +19,40 @@ var filtered = array.filter(function (el) {
 });
 
 console.log(filtered);
+
+
+//convert object values in array
+let d={
+  x:1,
+  y:2
+}
+
+let list= Object.keys(d)
+console.log(list)
+
+// Sorting array elements
+
+let a=[1,2,13,4]
+let b=[2,3,4,5,6,7]
+
+const c=[a,b]
+console.log(c.flat().sort((a,b)=> b-a))
+
+
+add(1,2)
+add(1)(2)
+
+function add(a,b){
+  if( a &&b){
+    return a+b
+  }
+  else {
+    return function (b){
+      return a+b
+    }
+  }
+  
+}
+
+
+console.log(add(1)(2))
